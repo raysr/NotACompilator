cd8e5eefbb778a2dc0b872ad4b5d47e4a011e214
raysr <raysamram@protonmail.com>
2018-11-20 19:38:39 +0100

    finished quadr and ts

--- COMP/Quad.h
+++ COMP/Quad.h
@@ -1,18 +1,43 @@
 /*
-#include<stdio.h>
-#include<string.h>
+#ifndef NULL
+
+   #ifdef __cplusplus
+
+      #define NULL 0
+
+   #else
+
+      #define NULL ((void *)0)
+
+   #endif
+
+#endif
 */
 typedef struct qdr{
 
 
 
+
+
+
+
     char oper[100];
+
+
 
     char op1[100];
 
+
+
     char op2[100];
 
+
+
     char res[100];
+
+
+
+
 
 
 
@@ -20,31 +45,67 @@ typedef struct qdr{
 
 
 
+
+
+
+
 typedef struct Element Element;
+
+
 
 struct Element
 
+
+
 {
+
+
 
     qdr quad;
 
+
+
     Element *suivant;
 
+
+
 };
+
+
 
 typedef struct Pile Pile;
 
+
+
 struct Pile
+
+
 
 {
 
+
+
     Element *premier;
+
+
 
 };
 
 
+	Pile pileQuads;
 
-Pile pileQuads;
+
+Pile *initialiser()
+
+{
+    Pile *pile = malloc(sizeof(*pile));
+    pile->premier = NULL;
+}
+
+
+
+
+
 
 int taille=0;
 
@@ -52,169 +113,135 @@ int taille=0;
 
 
 
+
+
+
+
+
+
 void empiler(Pile *pile, qdr nvQdr)
+
+
 
 {
 
-    Element *nouveau = malloc(sizeof(*nouveau));
+
+
+        Element *nouveau = malloc(sizeof(*nouveau));
+
+
 
     if (pile == NULL || nouveau == NULL)
 
+
+
     {
+
+
 
         exit(0);
 
+
+
     }
+
+
+
+
 
 
 
     nouveau->quad = nvQdr;
 
+
+
     nouveau->suivant = pile->premier;
+
+
 
     pile->premier = nouveau;
 
+
+
 }
+
+
+
+
 
 
 
 qdr depiler(Pile *pile)
 
+
+
 {
+
+
 
     if (pile == NULL)
 
+
+
     {
+
+
 
         exit(0);
 
+
+
     }
+
+
+
+
 
 
 
     qdr quadro;
 
+
+
     Element *elementDepile = pile->premier;
+
+
+
+
 
 
 
     if (pile != NULL && pile->premier != NULL)
 
+
+
     {
+
+
 
         quadro = elementDepile->quad;
 
+
+
         pile->premier = elementDepile->suivant;
+
+
 
         free(elementDepile);
 
+
+
     }
+
+
+
+
 
 
 
     return quadro;
 
-}
 
-
-
-void afficherQuadruplets(Pile *pile)
-
-{
-
-    if (pile == NULL)
-
-    {
-
-        exit(0);
-
-    }
-
-    Element *actuel = pile->premier;
-
-printf("*********************Les Quadruplets***********************\n");
-
-    while (actuel != NULL)
-
-    {
-
-        afficherQuad(actuel->quad);
-
-        actuel = actuel->suivant;
-
-    }
-
-    printf("\n");
-
-}
-
-
-
-
-
-void quadr(char opr[],char op1[],char op2[],char res[])
-
-{
-
-  qdr q;
-
-	strcpy(q.oper,opr);
-
-	strcpy(q.op1,op1);
-
-	strcpy(q.op2,op2);
-
-	strcpy(q.res,res);
-
-  empiler(&pileQuads,q);
-
-  taille=taille+1;
-
-}
-
-
-
-void ajour_quad(int num_quad, int colon_quad, char val [])
-
-{
-
-  int i=0;
-
-  Pile *p;
-
-  Element *e;
-  qdr q;
-
-  while(i!=(taille-num_quad))
-
-  {
-
-    q=depiler(&pileQuads);
-    empiler(&p,q);
-
-    i=i+1;
-
-  }
-
-   q=depiler(&pileQuads);
-
-if (colon_quad==1) strcpy(q.oper,val);
-
-else if (colon_quad==2) strcpy(q.op1,val);
-
-         else if (colon_quad==3) strcpy(q.op2,val);
-
-                   else if (colon_quad==4) strcpy(q.res,val);
-
-    empiler(&p,q);
-
-      while(p!=NULL)
-      {
-        q=depiler(&p);
-          empiler(&pileQuads,q);
-
-
-      }
 
 }
 
@@ -224,30 +251,196 @@ else if (colon_quad==2) strcpy(q.op1,val);
 
 int afficherQuad(qdr Q)
 
+
+
 {
 
- printf("\n - ( %s  ,  %s  ,  %s  ,  %s )",Q.oper,Q.op1,Q.op2,Q.res);
+
+
+ printf(" ( %s  ,  %s  ,  %s  ,  %s )",Q.oper,Q.op1,Q.op2,Q.res);
+
+
 
  printf("\n--------------------------------------------------------\n");
+
  return 0;
 
+
+
 }
 
-/*
-int main()
+
+
+
+
+void afficherQuadruplets(Pile *pile)
+
 {
-    qdr q;
 
-    quadr("ANZ","A1","B1","A1");
-    quadr("BNZ","A2","B2","A2");
-    quadr("CNZ","A3","B3","A3");
-    quadr("DNZ","A4","B4","A4");
-    quadr("ENZ","A5","B5","A5");
-    ajour_quad(2,2,"TEST");
-    afficherQuadruplets(&pileQuads);
+    if (pile == NULL)
+    {
 
-    return 0;
+
+
+        exit(0);
+
+
+
+    }
+
+
+
+    Element *actuel = pile->premier;
+
+int i=0;
+
+printf("\n*********************Les Quadruplets***********************\n");
+
+
+
+    while (actuel != NULL)
+    {
+
+
+printf("%d - ",taille-i);
+        afficherQuad(actuel->quad);
+        actuel = actuel->suivant;
+
+i=i+1;
+
+    }
+
+    printf("\n");
+
+
+
 }
 
-*/
 
+
+
+
+
+
+
+
+
+
+void quadr(char opr[],char op1[],char op2[],char res[])
+{
+ 
+  qdr q;
+	strcpy(q.oper,opr);
+
+
+
+	strcpy(q.op1,op1);
+
+
+
+	strcpy(q.op2,op2);
+
+
+
+	strcpy(q.res,res);
+
+
+
+  empiler(&pileQuads,q);
+
+
+
+  taille=taille+1;
+
+
+
+}
+
+
+
+
+
+
+
+void ajour_quad(int num_quad, int colon_quad, char val [])
+
+{
+
+
+  int i=0;
+
+
+
+  Pile *p;
+
+
+
+  Element *e;
+
+  qdr q;
+
+
+
+  while(i!=(taille-num_quad))
+  {
+
+
+
+    q=depiler(&pileQuads);
+
+    empiler(&p,q);
+
+
+
+    i=i+1;
+
+
+
+  }
+
+
+
+
+   q=depiler(&pileQuads);
+
+
+
+if (colon_quad==1) strcpy(q.oper,val);
+
+
+
+else if (colon_quad==2) strcpy(q.op1,val);
+
+
+
+         else if (colon_quad==3) strcpy(q.op2,val);
+
+
+
+                   else if (colon_quad==4) strcpy(q.res,val);
+
+
+
+    empiler(&p,q);
+
+
+i=0;
+      while(i<(taille-num_quad+1))
+      {
+
+
+
+        q=depiler(&p);
+
+          empiler(&pileQuads,q);
+
+
+
+i++;
+
+      }
+
+
+
+
+}
--- COMP/Syntaxe.tab.h
+++ COMP/Syntaxe.tab.h
@@ -1,23 +1,24 @@
-
-/* A Bison parser, made by GNU Bison 2.4.1.  */
+/* A Bison parser, made by GNU Bison 2.3.  */
 
 /* Skeleton interface for Bison's Yacc-like parsers in C
-   
-      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
    Free Software Foundation, Inc.
-   
-   This program is free software: you can redistribute it and/or modify
+
+   This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-   
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -28,10 +29,9 @@
    special exception, which will cause the skeleton and the resulting
    Bison output files to be licensed under the GNU General Public
    License without this special exception.
-   
+
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
-
 
 /* Tokens.  */
 #ifndef YYTOKENTYPE
@@ -40,62 +40,72 @@
       know about them.  */
    enum yytokentype {
      mc_Algo = 258,
-     mc_If = 259,
-     mc_Execut = 260,
-     mc_while = 261,
-     bibl_tab = 262,
-     bibl_boucle = 263,
-     bibl_calcul = 264,
-     SEP = 265,
-     mc_Deb = 266,
-     mc_Fin = 267,
-     mc_Si = 268,
-     mc_Faire = 269,
-     mc_Fait = 270,
-     mc_Pour = 271,
-     mc_Jusque = 272,
-     mc_Var = 273,
-     mc_INT = 274,
-     mc_REAL = 275,
-     mc_STRING = 276,
-     mc_CHAR = 277,
-     IDF = 278,
-     OPREL = 279,
-     AFF = 280,
-     PLUS = 281,
-     SUST = 282,
-     MULT = 283,
-     DIV = 284,
-     CONST_ENT = 285,
-     CONST_REAL = 286,
-     String = 287
+     mc_Deb = 259,
+     mc_If = 260,
+     mc_Execut = 261,
+     mc_while = 262,
+     mc_CONST = 263,
+     bibl_tab = 264,
+     bibl_boucle = 265,
+     bibl_calcul = 266,
+     SEP = 267,
+     mc_INT = 268,
+     mc_REAL = 269,
+     IDF = 270,
+     OPREL = 271,
+     AFF = 272,
+     PLUS = 273,
+     SUST = 274,
+     MULT = 275,
+     DIV = 276,
+     CONST_ENT = 277,
+     CONST_REAL = 278,
+     String = 279
    };
 #endif
+/* Tokens.  */
+#define mc_Algo 258
+#define mc_Deb 259
+#define mc_If 260
+#define mc_Execut 261
+#define mc_while 262
+#define mc_CONST 263
+#define bibl_tab 264
+#define bibl_boucle 265
+#define bibl_calcul 266
+#define SEP 267
+#define mc_INT 268
+#define mc_REAL 269
+#define IDF 270
+#define OPREL 271
+#define AFF 272
+#define PLUS 273
+#define SUST 274
+#define MULT 275
+#define DIV 276
+#define CONST_ENT 277
+#define CONST_REAL 278
+#define String 279
+
 
 
 
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
 typedef union YYSTYPE
+#line 42 "Syntaxe.y"
 {
-
-/* Line 1676 of yacc.c  */
-#line 19 "Syntaxe.y"
-
 int entier;
 float reel;
 char* str;
 char car;
-   
-
-
-/* Line 1676 of yacc.c  */
-#line 93 "Syntaxe.tab.h"
-} YYSTYPE;
-# define YYSTYPE_IS_TRIVIAL 1
+   }
+/* Line 1529 of yacc.c.  */
+#line 104 "Syntaxe.tab.h"
+	YYSTYPE;
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
 #endif
 
 extern YYSTYPE yylval;
-
 
